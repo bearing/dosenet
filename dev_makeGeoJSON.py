60698043c73e9ab2371d1da65013066f07532b21
Navrit Bal <navritbal@gmail.com>
2015-04-06 20:37:56 -0700

    Clean up & reorganization

--- dev_makeGeoJSON.py
+++ dev_makeGeoJSON.py
@@ -5,7 +5,7 @@
 # Nuclear Engineering 170A: Nuclear Design
 # University of California, Berkeley, U.S.A.
 # Created: Sat 21/02/15
-# Last updated: Mon 23/03/15
+# Last updated: Mon 06/04/15
 
 import MySQLdb as mdb
 import sys
@@ -20,40 +20,122 @@ from plotly.graph_objs import *
 import pandas as pd
 import time
 import datetime
-
-# Plot.ly sign in using
-py.sign_in('ne170','ilo0p1671e')
-# Open database connection
-db = mdb.connect("localhost","ne170group","ne170groupSpring2015","dosimeter_network")
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
-
-secondsInYear = 31557600 #365.23 days
-secondsInMonth = 2592000 #30 days
-secondsInWeek = 604800 #7 days
-secondsInDay = 86400 #24 hours
-secondsInHour = 3600 #60 minutes
-secondsInMinute = 60 # Duh
-
-feature_list=[]
-dtRow_rowarray_list = []
-dosesForEachStation_1d_list =[]
-station_row = 0
-fname = ''
+#from joblib import Parallel, delayed  
+#import multiprocessing
 
 
-# Get # of stations, station name, longitude, latitude
-cursor.execute("SELECT `ID`, `Name`, `Lat`, `Long` FROM dosimeter_network.stations;")
-station_rows = cursor.fetchall()
-# Convert query to row arrays
-station_rowarray_list = []
+def initialise():
+	#global num_cores = multiprocessing.cpu_count()
+	# Open database connection
+	global db
+	db = mdb.connect("localhost","ne170group","ne170groupSpring2015","dosimeter_network")
+	# prepare a cursor object using cursor() method
+	global cursor
+	cursor = db.cursor()
+	# Plot.ly sign in using
+	py.sign_in('ne170','ilo0p1671e')
+	global station_rowarray_list
+	station_rowarray_list = []
 
-for station_row in station_rows:
-    s = (station_row[0], station_row[1], station_row[2], station_row[3])
-    #print s
-    station_rowarray_list.append(s)
+def getStationInfoFromDB():
+	# Get # of stations, station name, longitude, latitude
+	cursor.execute("SELECT `ID`, `Name`, `Lat`, `Long` FROM dosimeter_network.stations;")
+	global station_rows
+	station_rows = cursor.fetchall()
+	# Convert query to row arrays
 
-for station_row in range(len(station_rows)):
+def setStationInfo(i):
+	station_rowarray_list.append((i[0], i[1], i[2], i[3]))
+	return
+
+def setStationInfoForAll():
+	for i in station_rows:
+		setStationInfo(i)
+
+"""
+Parallel(n_jobs=num_cores)(delayed(setStationInfo(i)) for i in station_rows)
+"""
+
+def setConstants():
+	global secondsInYear
+	secondsInYear = 31557600 #365.23 days
+	global secondsInMonth
+	secondsInMonth = 2592000 #30 days
+	global secondsInWeek
+	secondsInWeek = 604800 #7 days
+	global secondsInDay
+	secondsInDay = 86400 #24 hours
+	global secondsInHour
+	secondsInHour = 3600 #60 minutes
+	global secondsInMinute
+	secondsInMinute = 60 # Duh
+
+def initVariables():
+	global feature_list
+	feature_list=[]
+	global dtRow_rowarray_list
+	dtRow_rowarray_list = []
+	global dosesForEachStation_1d_list
+	dosesForEachStation_1d_list =[]
+	global station_row
+	station_row = 0
+	global fname
+	fname = ''
+	#global cnt = 0
+	#cnt = 0
+
+def sqlForPlot(startTime,endTime):
+	sqlString = "SELECT receiveTime, cpm, rem, rad FROM dosnet INNER JOIN stations ON dosnet.stationID=stations.ID WHERE `stations`.`Name`='%s' AND receiveTime BETWEEN '%s' AND '%s';" % (stationID, startTime, endTime)
+	#print sqlString
+	try:
+		cursor.execute(sqlString)
+	except Exception, e:
+		print 'sqlString failure:' + str(e)
+	dosePerStation = cursor.fetchall()
+	#
+	# Populate time-restricted row array list
+	# Panda code to get SQL tuples of tuples into panda df tables which are WAAAY easier to work with (and apparently are faster?)
+	# I'm doing this purely because the other more manual method did not work
+	# Pandas are easier to work with
+	df = pd.DataFrame( [[ij for ij in i] for i in dosePerStation] )
+	#sys.getsizeof(df_1d)
+	df.rename(columns={0:'receiveTime', 1:'CPM', 2:'rem', 3:'rad'}, inplace=True)
+	return df
+
+def makePlot(stationID,past_when,unit,dfunit,plotTitle,df):
+	plength = str(past_when) # plot length string
+	# [unit] over numberOfSeconds for a specific named station [LstationID]
+	try:
+		# make the filename for Plot.ly export
+		# current station ID
+		fname = (str(stationID)+'_'+unit+'_'+plength)
+		# setup the plot.ly plot type as a Scatter with points with panda DataFrames (df)
+		trace = Scatter( 
+			x=df['receiveTime'],
+			y=df[dfunit], # changes depending on which units you are plotting
+			mode='markers'
+		)
+		fontPref = Font(family='Arial, monospace',size=16,color='#000000')
+		layout = Layout(title=(str(stationID)+' '+unit+' '+plength),xaxis=XAxis(title='Time',rangemode='nonzero',autorange=True),yaxis=YAxis( type='log',title=plotTitle),font=fontPref)
+		# Plot.ly export, doesn't open a firefox window on the server
+		plot_url = (py.plot(Figure(data=Data([trace]), layout=layout), filename=fname, auto_open=False))
+	except:
+		print 'This '+unit+' failed - %s' % filename
+
+def getNumberOfStations():
+	return range(len(station_rows))
+
+#######################################################
+## Iterate through to define all features (stations) ##
+#######################################################
+initialise()
+getStationInfoFromDB()
+setStationInfoForAll()
+setConstants()
+initVariables()
+
+
+for station_row in getNumberOfStations():
 	#cnt = cnt+1
 	#print cnt
 	# builds up a tuple coordinates of the stations, longitude & latitude
@@ -68,6 +150,7 @@ for station_row in range(len(station_rows)):
 	sqlString = ""
 	dtRows = cursor.fetchall()
 	for dtRow in dtRows:
+		print stationID
 		#print dtRow[0]
 		#t = (dtRow[0], dtRow[1])
 		t = (dtRow[0],dtRow[1],dtRow[2],dtRow[3],dtRow[4])
@@ -115,7 +198,6 @@ for station_row in range(len(station_rows)):
 			####### HOUR #########
 			plength = 'Past_Hour' # plot length string
 			# CPM over numberOfSeconds for all stations
-			print 'CPM over numberOfSeconds for all stations'
 			try:
 				fname = (str(LstationID)+'_'+'CPM'+'_'+plength)
 				trace = Scatter(
@@ -133,7 +215,6 @@ for station_row in range(len(station_rows)):
 				print 'This CPM failed - %s' % filename
 			#
 			# REM over numberOfSeconds for all stations
-			print 'REM over numberOfSeconds for all stations'
 			try:
 				fname = (str(LstationID)+'_'+'REM'+'_'+plength)
 				trace = Scatter(
@@ -151,7 +232,8 @@ for station_row in range(len(station_rows)):
 				print 'plot REM failed - %s' % filename
 			#
 			# RAD over numberOfSeconds for all stations
-			print 'RAD over numberOfSeconds for all stations'
+			#
+			# CHANGE TO microSievert PER HOUR!!
 			try:
 				fname = (str(LstationID)+'_'+'RAD'+'_'+plength)
 				trace = Scatter(
@@ -168,8 +250,6 @@ for station_row in range(len(station_rows)):
 			except:
 				print 'plot RAD failed - %s' % filename
 			try:
-				print 'Define feature etc.'
-				#feature = Feature(geometry=point,properties={"Name": stationID, "Latest dose (CPM)": LCPM, "Latest dose (mrem)": Lrem, "Latest dose (mrad)": Lrad,"Latest measurement": str(LTime), "1 day URL":plot_url})				
 				feature = Feature(geometry=point,properties={("CPM_URL"+"_"+plength):plot_url_cpm, ("REM_URL"+"_"+plength):plot_url_rem, ("RAD_URL"+"_"+plength):plot_url_rad, "Latest dose (CPM)": LCPM ,"Latest measurement": str(LTime), "Name": stationID})
 				feature_list.append(feature)
 			except Exception as e:
@@ -213,7 +293,6 @@ for station_row in range(len(station_rows)):
 			####### HOUR #########
 			plength = 'Past_Day' # plot length string
 			# CPM over numberOfSeconds for all stations
-			print 'CPM over numberOfSeconds for all stations'
 			try:
 				fname = (str(LstationID)+'_'+'CPM'+'_'+plength)
 				trace = Scatter(
@@ -231,7 +310,6 @@ for station_row in range(len(station_rows)):
 				print 'plot CPM failed - %s' % filename
 			#
 			# REM over numberOfSeconds for all stations
-			print 'REM over numberOfSeconds for all stations'
 			try:
 				fname = (str(LstationID)+'_'+'REM'+'_'+plength)
 				trace = Scatter(
@@ -249,7 +327,6 @@ for station_row in range(len(station_rows)):
 				print 'plot REM failed - %s' % filename
 			#
 			# RAD over numberOfSeconds for all stations
-			print 'RAD over numberOfSeconds for all stations'
 			try:
 				fname = (str(LstationID)+'_'+'RAD'+'_'+plength)
 				trace = Scatter(
@@ -266,7 +343,6 @@ for station_row in range(len(station_rows)):
 			except:
 				print 'plot RAD failed - %s' % filename
 			try:
-				print 'Define feature etc.'
 				#feature = Feature(geometry=point,properties={"Name": stationID, "Latest dose (CPM)": LCPM, "Latest dose (mrem)": Lrem, "Latest dose (mrad)": Lrad,"Latest measurement": str(LTime), "1 day URL":plot_url})				
 				feature = Feature(geometry=point,properties={("CPM_URL"+"_"+plength):plot_url_cpm, ("REM_URL"+"_"+plength):plot_url_rem, ("RAD_URL"+"_"+plength):plot_url_rad, "Latest dose (CPM)": LCPM ,"Latest measurement": str(LTime), "Name": stationID})
 				feature_list.append(feature)
@@ -307,7 +383,6 @@ for station_row in range(len(station_rows)):
 			####### MONTH #########
 			plength = 'Past_Month' # plot length string
 			# CPM over numberOfSeconds for all stations
-			print 'CPM over numberOfSeconds for all stations'
 			try:
 				fname = (str(LstationID)+'_'+'CPM'+'_'+plength)
 				trace = Scatter(
@@ -325,7 +400,6 @@ for station_row in range(len(station_rows)):
 				print 'plot CPM failed'
 			#
 			# REM over numberOfSeconds for all stations
-			print 'REM over numberOfSeconds for all stations'
 			try:
 				fname = (str(LstationID)+'_'+'REM'+'_'+plength)
 				trace = Scatter(
@@ -343,7 +417,6 @@ for station_row in range(len(station_rows)):
 				print 'plot REM failed'
 			#
 			# RAD over numberOfSeconds for all stations
-			print 'RAD over numberOfSeconds for all stations'
 			try:
 				fname = (str(LstationID)+'_'+'RAD'+'_'+plength)
 				trace = Scatter(
@@ -360,7 +433,6 @@ for station_row in range(len(station_rows)):
 			except:
 				print 'plot RAD failed'
 			try:
-				print 'Define feature etc.'
 				#feature = Feature(geometry=point,properties={"Name": stationID, "Latest dose (CPM)": LCPM, "Latest dose (mrem)": Lrem, "Latest dose (mrad)": Lrad,"Latest measurement": str(LTime), "1 day URL":plot_url})				
 				feature = Feature(geometry=point,properties={("CPM_URL"+"_"+plength):plot_url_cpm, ("REM_URL"+"_"+plength):plot_url_rem, ("RAD_URL"+"_"+plength):plot_url_rad, "Latest dose (CPM)": LCPM ,"Latest measurement": str(LTime), "Name": stationID})
 				feature_list.append(feature)
@@ -374,6 +446,41 @@ for station_row in range(len(station_rows)):
 			print 'Plotting failed'
 			print e
 featurecollection = FeatureCollection(feature_list)
+##########################################################
+# matplotlib plotting to screen
+
+'''# PLOT REM VS TIME
+title = fname
+plt.plot(df['receiveTime'],df['rem'])
+#plt.plot(dosesForEachStation_1d_list[0][:],dosesForEachStation_1d[2][:]) #Plot CPM (y) vs Time (x)
+plt.xlabel(r'$Time$',fontsize=24) # X Y labels
+plt.ylabel(r'$REM/hour$',fontsize=24)
+plt.savefig(title+'.svg', format='svg')'''
+
+
+#title = (t[4]+' starting at '+str(startPlotTime)) # t[4] --> stationID
+#plt.plot(df_1d['receiveTime'],df_1d['CPM'])
+###plt.plot(dosesForEachStation_1d_list[0][:],dosesForEachStation_1d_list[1][:]) #Plot CPM (y) vs Time (x)
+#plt.xlabel(r'$Time$',fontsize=24) # X Y labels
+#plt.ylabel(r'$CPM$',fontsize=24)
+#plt.savefig(title+'.svg', format='svg')
+#plt.show()
+
+
+'''
+#receiveString = str(df['receiveTime'][0])
+receiveString = str(receiveTime)
+#title = ('Dose (CPM) over time starting '+receiveString)
+title = (t[4]+' starting at '+receiveString)
+plt.title(title,fontsize=16)
+plt.plot(df['receiveTime'],df['CPM'])
+plt.ylabel(r'$CPM$',fontsize=24)
+plt.xlabel(r'$Time$',fontsize=24)
+
+plt.savefig(title+'.svg', format='svg')
+#plt.show()
+'''
+
 
 # Export dump to GeoJSON file
 dump = geojson.dumps(featurecollection)
--- write_to_db-virtual_dosimeter.py
+++ write_to_db-virtual_dosimeter.py
@@ -7,12 +7,13 @@
 # Nuclear Engineering 170A: Nuclear Design
 # University of California, Berkeley, U.S.A.
 # Created: Sun 1/03/15
-# Last updated: Wed 11/03/15
+# Last updated: Fri 13/03/15
 
 import MySQLdb as mdb
 import time
 from random import randrange, randint, random
 import datetime
+import math
 #connect to db
 db = mdb.connect("localhost","ne170group","ne170groupSpring2015","dosimeter_network")
 
@@ -20,46 +21,54 @@ db = mdb.connect("localhost","ne170group","ne170groupSpring2015","dosimeter_netw
 cursor = db.cursor()
 
 stationID= 1
-cpm = 1
+cpm = 1.0
 errorFlag = 1
+rem = 1.0
+rad = 1.0
+sqlstring =""
 #one_min = datetime.timedelta(minutes=1)
 #one_hour = datetime.timedelta(hours=1) #not used yet
 #one_day = datetime.timedelta(days=1) #not used yet
 #one_week = datetime.timedelta(weeks=1) #not used yet - Does actually work
-receiveTime = datetime.datetime.now() + datetime.timedelta(hours=-3) #+ 22*one_min
+receiveTime = datetime.datetime.now() + datetime.timedelta(hours=-1) #+ 22*one_min
 
 
 # INCREMENT TIME t [minutes]
-for t in range(0,10080): # 0,60 --> 1 hr #0,1440 --> 1 day #0,10080 --> 1 week
+for t in range(0,60): # 0,60 --> 1 hr #0,1440 --> 1 day #0,10080 --> 1 week
 	# TIME INCREMENT --> add 1 minute
 	#receiveTime = receiveTime.replace(microsecond=0) + one_min
 	receiveTime = receiveTime.replace(microsecond=0)
+	receiveTime = receiveTime + datetime.timedelta(minutes=1)
 	#insert to table
-	for i in range(0,5):
+	for i in range(1,89):
 		try:
-			receiveTime = receiveTime + datetime.timedelta(seconds=randint(0,i))
 			stationID = i
 			# Make realistic
 			# FIND OUT WHAT IS REALISTIC
-			cpm = randint(0,(i*10)) + random()*3
+			# cpm = str(abs(5*random()* (math.sin(i)* 10. ** -6)))
+			cpm = abs(100+random()* (math.sin(i)* 10. ** 1))
 			#conversion from cpm to rem - apparently correct
 			#####################################
 			# NEEDS TO BE CONFIRMED  # ASK RYAN #
 			#####################################
-			rem = cpm * 8.76
+			rem = cpm * 1200/60/1000 # FOR CAESIUM-137 #per hour # milliRem
 			#############################
 			# GET the ACTUAL CONVERSION #
 			#############################
 			#conversion from cpm to rad
-			rad = rem * 0.1
+			rad = rem
 			errorFlag = randint(0,1)
-			#print stationID, errorFlag, cpm, rem, rad
+			#print receiveTime, stationID, cpm, rem, rad, errorFlag
 			#print("---------")
 			# without time
 			# cursor.execute("""INSERT INTO dosnet(stationID, cpm, rem, rad, errorFlag) VALUES (%s,%s,%s,%s,%s);""",(stationID,cpm,rem,rad,errorFlag))
 			# WITH time
-			cursor.execute("""INSERT INTO dosnet(receiveTime,stationID, cpm, rem, rad, errorFlag) VALUES (%s,%s,%s,%s,%s,%s);""",(receiveTime,stationID,cpm,rem,rad,errorFlag))
-			db.commit()
+			try:
+				cursor.execute("""INSERT INTO dosnet(receiveTime, stationID, cpm, rem, rad, errorFlag) VALUES (%s,%s,%s,%s,%s,%s);""",(receiveTime, stationID,cpm,rem,rad,errorFlag))
+				db.commit()
+			except Exception, e:
+				print "SQL query is wrong"
+				print str(e)
 			# No need to add delay to the simulation --> we are now incrementing time in the outer for loop
 			#time.sleep(0.1)
 		except:
