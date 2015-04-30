2373ff5e43870ddc2e337b32e03be3628656ee08
Navrit Bal <navritbal@gmail.com>
2015-04-30 15:11:23 -0700

    Get the grim IP

---
+++ getGrimIP.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Navrit Bal
+# Thursday 30 April 2015
+# GET IP ADDRESS OF GRIM.NUC.BERKELEY.EDU
+# From http://pymotw.com/2/socket/addressing.html - Looking Up Server Addresses
+
+import socket
+for response in socket.getaddrinfo('grim.nuc.berkeley.edu', 'http'):
+    # Unpack the response tuple
+    family, socktype, proto, canonname, sockaddr = response
+    print 'Protocol: ', proto # Should be UDP if correct, could be TCP
+    print 'Address, port: ', sockaddr # Should be like this format ('IP Address',port)- eg. ('128.32.192.205',80)
