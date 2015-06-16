aeb7e8d0babde52d53010a0a50cb0a377448e5a6
Navrit Bal <navritbal@gmail.com>
2015-06-16 15:03:19 -0700

    Removed semicolons
    Ryan —> Navrit

--- udp_injector.py
+++ udp_injector.py
@@ -1,29 +1,28 @@
-import sys;
-import os;
-import_list=['crypt','mysql','udp'];
+import sys
+import os
+import_list=['crypt','mysql','udp']
 for el in import_list:
-    sys.path.append( os.path.abspath(os.path.join(os.getcwd(),el)) );
-from crypt import cust_crypt as ccrypt;
-from mysql import mysql_tools as msqlt
+    sys.path.append( os.path.abspath(os.path.join(os.getcwd(),el)) )
+from crypt import cust_crypt as ccrypt
+from mysql import mysql_tools as mysql
 from udp import udp_tools as udpt
 
-key_file_lst=['/home/dosenet/.ssh/id_rsa_dosenet'];
-de=ccrypt.public_d_encrypt(key_file_lst=key_file_lst);
+privateKey = ['/home/dosenet/.ssh/id_rsa_dosenet']
+de = ccrypt.public_d_encrypt(key_file_lst = privateKey)
 
 #connect
-db=msqlt.SQLObject();
-print db;
+db = mysql.SQLObject()
 
-UDP_IP = "192.168.1.101"
-UDP_PORT = 5005
-sock=udpt.custSocket(ip=UDP_IP,port=UDP_PORT,decrypt=de);
+GRIM = "192.168.1.101" # GRIM (DB server) local IP
+port = 5005 # Arbitrary, non-conflicting port
+sock = udpt.custSocket(ip=GRIM,port=port,decrypt=de)
 
 while True:
     try:
-        data = sock.listen();
-        print "received message:", data
-        db.inject(data);
-    except KeyboardInterrupt:
+        data = sock.listen()
+        print "Received: ", data
+        db.inject(data)
+    except (KeyboardInterrupt, SystemExit):
         print "Exiting cleaning"
         del db
         sys.exit(0)
