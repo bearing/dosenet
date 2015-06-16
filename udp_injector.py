7a672cdec6d8207f67109d372db50d73e4f2c4ef
Navrit Bal <navritbal@gmail.com>
2015-06-16 15:03:41 -0700

    Revert "Removed semicolons"
    This reverts commit aeb7e8d0babde52d53010a0a50cb0a377448e5a6.

--- udp_injector.py
+++ udp_injector.py
@@ -1,28 +1,29 @@
-import sys
-import os
-import_list=['crypt','mysql','udp']
+import sys;
+import os;
+import_list=['crypt','mysql','udp'];
 for el in import_list:
-    sys.path.append( os.path.abspath(os.path.join(os.getcwd(),el)) )
-from crypt import cust_crypt as ccrypt
-from mysql import mysql_tools as mysql
+    sys.path.append( os.path.abspath(os.path.join(os.getcwd(),el)) );
+from crypt import cust_crypt as ccrypt;
+from mysql import mysql_tools as msqlt
 from udp import udp_tools as udpt
 
-privateKey = ['/home/dosenet/.ssh/id_rsa_dosenet']
-de = ccrypt.public_d_encrypt(key_file_lst = privateKey)
+key_file_lst=['/home/dosenet/.ssh/id_rsa_dosenet'];
+de=ccrypt.public_d_encrypt(key_file_lst=key_file_lst);
 
 #connect
-db = mysql.SQLObject()
+db=msqlt.SQLObject();
+print db;
 
-GRIM = "192.168.1.101" # GRIM (DB server) local IP
-port = 5005 # Arbitrary, non-conflicting port
-sock = udpt.custSocket(ip=GRIM,port=port,decrypt=de)
+UDP_IP = "192.168.1.101"
+UDP_PORT = 5005
+sock=udpt.custSocket(ip=UDP_IP,port=UDP_PORT,decrypt=de);
 
 while True:
     try:
-        data = sock.listen()
-        print "Received: ", data
-        db.inject(data)
-    except (KeyboardInterrupt, SystemExit):
+        data = sock.listen();
+        print "received message:", data
+        db.inject(data);
+    except KeyboardInterrupt:
         print "Exiting cleaning"
         del db
         sys.exit(0)
