8200da62b99d54a2982536747af5c4c8047c1c8b
Navrit Bal <navritbal@gmail.com>
2015-06-16 15:57:10 -0700

    Clean up
    Removed semicolons, added spaces, renamed variables, added exception
    handling & commenting

--- udp_injector.py
+++ udp_injector.py
@@ -1,31 +1,44 @@
-import sys;
-import os;
-import_list=['crypt','mysql','udp'];
+#!/usr/bin/python
+# 
+# Ryan Pavlovsky (until Mon Jun 15 2015)
+# Navrit Bal (after Jun 15 2015)
+# DoseNet
+# Applied Nuclear Physics Division
+# Lawrence Berkeley National Laboratory, Berkeley, U.S.A.
+# Adapted from udp_injector.py (Ryan Pavlovsky)
+# Last updated: Tue 16/06/15
+#################
+## Run on GRIM ##
+#################
+import sys
+import os
+# Extensible way for adding future imports
+import_list = ['crypt','mysql','udp']
 for el in import_list:
-    sys.path.append( os.path.abspath(os.path.join(os.getcwd(),el)) );
-from crypt import cust_crypt as ccrypt;
-from mysql import mysql_tools as msqlt
-from udp import udp_tools as udpt
+    sys.path.append( os.path.abspath(os.path.join(os.getcwd(),el)) )
+from crypt import cust_crypt as ccrypt
+from mysql import mysql_tools as mySQLTool
+from udp import udp_tools as udpTool
 
-key_file_lst=['/home/dosenet/.ssh/id_rsa_dosenet'];
-de=ccrypt.public_d_encrypt(key_file_lst=key_file_lst);
+# Initialise decryption & database objects
+privateKey = ['/home/dosenet/.ssh/id_rsa_dosenet']
+de = ccrypt.public_d_encrypt(key_file_lst=privateKey) # Uses 1 private key ()
+db = mySQLTool.SQLObject()
+# Set up network information >> points to GRIM's internal static IP address at port 5005
+GRIM = "192.168.1.101"
+port = 5005
+sock = udpTool.custSocket(ip=GRIM,port=port,decrypt=de)
 
-#connect
-db=msqlt.SQLObject();
-print db;
-
-UDP_IP = "192.168.1.101"
-UDP_PORT = 5005
-sock=udpt.custSocket(ip=UDP_IP,port=UDP_PORT,decrypt=de);
-
+# Runs until keyboard interrupt or system exit 
 while True:
     try:
-        data = sock.listen();
+        data = sock.listen()
         print "received message:", data
-        db.inject(data);
-    except KeyboardInterrupt:
-        print "Exiting cleaning"
-        del db
+        db.inject(data)
+    except (KeyboardInterrupt, SystemExit), e:
+        print "Exit cleaning"
+        raise e
+        del db # Manual garbage collection
         sys.exit(0)
     except:
-        print "Cannot decrypt data..."
+        print "Exception: Cannot decrypt data..."
