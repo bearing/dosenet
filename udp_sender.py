4591b26a093d16d7550fa3a8de8c4a761a698e31
Navrit Bal <navritbal@gmail.com>
2015-06-23 18:20:48 -0700

    Authentication stage
    Implementing the data pipeline, package updates (NOT SENDING RPi TIME)

--- udp_sender.py
+++ udp_sender.py
@@ -7,39 +7,51 @@ import time
 from time import sleep
 from dosimeter import dosimeter
 import crypt.cust_crypt as ccrypt
+##########################
+## Run on Raspberry Pis ##
+##########################
 
 publicKey=['id_rsa_dosenet.pub']
 pe = ccrypt.public_d_encrypt(key_file_lst = publicKey)
 
-stationID = 1
-GRIM = 'grim.nuc.berkeley.edu'
+##################################
+# LOAD FROM CONFIG FILE          #
+stationID = 1                    #
+msgHash = ''                     #
+##################################
+IP = 'grim.nuc.berkeley.edu'
 port = 5005
-
-#print "UDP target IP:", GRIM
-#print "UDP target port:", port
+#print "UDP target IP @ port :", IP + ':' + port
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # uses UDP protocol
-det = dosimeter();  # Initialise dosimeter object from dosimeter.py
+det = dosimeter()  # Initialise dosimeter object from dosimeter.py
 
 def getDatetime():
     return datetime.datetime.now()
 
 while True:
-    cpm, cpmError = det.getCPMWithError()
-    errorCode = 0 # Default 'working' state - error code 0
-    #if len(det.counts) > 0: # Do not understand the purpose of this line
-	if (getDatetime() - det.counts[-1]).total_seconds() >= 300: #Sets how long of a period of zero counts until it's considered an error
-        errorCode = 12
-    # ¿ THIS IS REALLY POINTLESS ?
-    # MAYBE A MAXIMUM RATE OF INCREASE COULD BE USEFUL?
-    #if cpm >= 1000: #Sets maximum threshold value over which count rate is considered an error
-    #    errorCode = 66
-    time = getDatetime().strftime("%Y-%m-%d %H:%M:%S")
-    c = ','
-    package = str(stationID) +c+ time +c+ str(cpm) +c+ str(cpmError) +c+ str(errorCode)
-    #print packet
-    packet = pe.encrypt_message(package)[0]
-    #print packet
-    sock.sendto(packet, (GRIM, port))
-    print "Packet sent"
-    time.sleep(120)
+    try: 
+        cpm, cpmError = det.getCPMWithError()
+        errorCode = 0 # Default 'working' state - error code 0
+        datetime = getDatetime()
+        #if len(det.counts) > 0: # Do not understand the purpose of this line
+        if (datetime - det.counts[-1]).total_seconds() >= 300: #Sets how long of a period of zero counts until it's considered an error
+            errorCode = 12
+        # THIS IS REALLY POINTLESS ?
+        # MAYBE A MAXIMUM RATE OF INCREASE COULD BE USEFUL?
+        #if cpm >= 1000: #Sets maximum threshold value over which count rate is considered an error
+        #    errorCode = 66
+        #time = getDatetime().strftime("%Y-%m-%d %H:%M:%S")
+        c = ','
+        package = str(msgHash) +c+ str(stationID) +c+ str(cpm) +c+ str(cpmError) +c+ str(errorCode)
+        print packet
+        packet = pe.encrypt_message(package)[0]
+        print packet
+        sock.sendto(packet, (IP, port))
+        print "Packet sent @ " + datetime +' - '+ IP +':'+ port
+        time.sleep(120)
+    except (KeyboardInterrupt, SystemExit):
+        print '.... User interrupt ....\n Byyeeeeeeee'
+        sys.exit(0)
+    except Exception, e:
+        raise e
