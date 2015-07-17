48d44e0b55cc4c2ff0c6b69685effe44b88d0cb9
Navrit Bal <navritbal@gmail.com>
2015-07-17 13:38:48 -0700

    When udp_injector is already running

--- udp/udp_tools.py
+++ udp/udp_tools.py
@@ -9,7 +9,13 @@ class custSocket:
         if( not decrypt.private_key ):
             print 'Warning: You will not be able to decrypt messages'
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind((ip,port))
+        try:
+            self.sock.bind((ip,port))  
+        except Exception, e:
+            print '\n\t\t ~~ IP address already in use ~~'
+            print '\t\t ~~~~ This script is probably running already ~~~~'
+            print '\t ~~ If not, there\'s some network issue. Good luck... ~~'
+            raise e
 
     def listen(self):
         data, addr = self.sock.recvfrom(1024)
