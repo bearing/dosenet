cad69a11e38868ce373f0fa25487a9d43f28453f
Navrit Bal <navritbal@gmail.com>
2015-07-23 16:05:29 -0700

    Maybe this str() will help

--- mysql/mysql_tools.py
+++ mysql/mysql_tools.py
@@ -69,7 +69,7 @@ class SQLObject:
         print hash_list
         msg_hash = data[:32] # Is it the correct hash length?
         print msg_hash
-        if any(msg_hash in i for i in hash_list): # Verify the hash is in the list
+        if any(str(msg_hash) in i for i in hash_list): # Verify the hash is in the list
             print 'Hash is not in list'
             return False
         # Ok, we think this could be a real station
