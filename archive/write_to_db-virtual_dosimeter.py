2a64be9ec8632dc487e32c1e197eb71ef4e89011
Navrit Bal <navritbal@gmail.com>
2015-07-27 15:41:12 -0700

    Clean up & update
    Historical data injection for demo and website testing

--- archive/write_to_db-virtual_dosimeter.py
+++ archive/write_to_db-virtual_dosimeter.py
@@ -7,71 +7,53 @@
 # Nuclear Engineering 170A: Nuclear Design
 # University of California, Berkeley, U.S.A.
 # Created: Sun 1/03/15
-# Last updated: Fri 13/03/15
+# Last updated: Mon 27/06/15
 
 import MySQLdb as mdb
 import time
 from random import randrange, randint, random
 import datetime
 import math
-#connect to db
-db = mdb.connect("localhost","ne170group","ne170groupSpring2015","dosimeter_network")
 
-#setup cursor
+db = mdb.connect("localhost",
+				"ne170group",
+				"ne170groupSpring2015",
+				"dosimeter_network")
 cursor = db.cursor()
 
 stationID= 1
 cpm = 1.0
-errorFlag = 1
+errorFlag = 0
 rem = 1.0
 rad = 1.0
-sqlstring =""
-#one_min = datetime.timedelta(minutes=1)
-#one_hour = datetime.timedelta(hours=1) #not used yet
-#one_day = datetime.timedelta(days=1) #not used yet
-#one_week = datetime.timedelta(weeks=1) #not used yet - Does actually work
 receiveTime = datetime.datetime.now() + datetime.timedelta(hours=-1) #+ 22*one_min
 
-
-# INCREMENT TIME t [minutes]
-for t in range(0,60): # 0,60 --> 1 hr #0,1440 --> 1 day #0,10080 --> 1 week
-	# TIME INCREMENT --> add 1 minute
-	#receiveTime = receiveTime.replace(microsecond=0) + one_min
+for t in range(0, 1440): # 0,60 --> 1 hr #0,1440 --> 1 day #0,10080 --> 1 week
+	# TIME INCREMENT --> add 5 minutes
 	receiveTime = receiveTime.replace(microsecond=0)
-	receiveTime = receiveTime + datetime.timedelta(minutes=1)
-	#insert to table
-	for i in range(1,89):
+	receiveTime = receiveTime + datetime.timedelta(minutes=5)
+	for i in range(2,3): # Set one station at a time
 		try:
 			stationID = i
-			# Make realistic
-			# FIND OUT WHAT IS REALISTIC
-			# cpm = str(abs(5*random()* (math.sin(i)* 10. ** -6)))
-			cpm = abs(100+random()* (math.sin(i)* 10. ** 1))
-			#conversion from cpm to rem - apparently correct
-			#####################################
-			# NEEDS TO BE CONFIRMED  # ASK RYAN #
-			#####################################
-			rem = cpm * 1200/60/1000 # FOR CAESIUM-137 #per hour # milliRem
-			#############################
-			# GET the ACTUAL CONVERSION #
-			#############################
-			#conversion from cpm to rad
-			rad = rem
+			cpm = abs(random()* (math.sin(i)* 10. ** 1))
+			rem = cpm * 0.01886792 # mREM/hr
+			rad = cpm * 0.00980392 # uSv/hr
 			errorFlag = randint(0,1)
-			#print receiveTime, stationID, cpm, rem, rad, errorFlag
-			#print("---------")
 			# without time
 			# cursor.execute("""INSERT INTO dosnet(stationID, cpm, rem, rad, errorFlag) VALUES (%s,%s,%s,%s,%s);""",(stationID,cpm,rem,rad,errorFlag))
-			# WITH time
 			try:
-				cursor.execute("""INSERT INTO dosnet(receiveTime, stationID, cpm, rem, rad, errorFlag) VALUES (%s,%s,%s,%s,%s,%s);""",(receiveTime, stationID,cpm,rem,rad,errorFlag))
-				db.commit()
-			except Exception, e:
-				print "SQL query is wrong"
+				cursor.execute("INSERT INTO dosnet(receiveTime, stationID, \
+					cpm, rem, rad, errorFlag) VALUES (%s,%s,%s,%s,%s,%s);",\
+					(receiveTime, stationID,cpm,rem,rad,errorFlag)) # WITH time
+			except Exception as e:
+				print 'SQL query failed'
 				print str(e)
-			# No need to add delay to the simulation --> we are now incrementing time in the outer for loop
-			#time.sleep(0.1)
-		except:
-			print("Some exception")
-
-db.close()
+		except Exception as e:
+			raise e
+try:
+	db.commit()
+except Exception as e:
+	print 'Could not commit changes to database - no changes have been applied.'
+	raise e
+finally:
+	db.close()
