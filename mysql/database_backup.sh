801f2178161d2b96f34777831efa688d966484e1
Navrit Bal <navritbal@gmail.com>
2015-07-28 14:40:11 -0700

    Debugging and formatting change

--- mysql/database_backup.sh
+++ mysql/database_backup.sh
@@ -22,15 +22,17 @@ Google Drive /DoseNet/DatabaseAndNetworking/${GREEN}Networking Details.gdoc${NC}
 
 if [[ $CHOICE = "all" ]]; then
    printf "%s\n" "~ Backing up the whole dosimeter_network database"
-   printf "%b\n" "~~ Using: ${BLUE}`mysqldump -u ne170group -p dosimeter_network > ~/backup_all_dosenet.sql`${NC}"
+   printf "%b\n" "\e~~ Using: ${BLUE}mysqldump -u ne170group -p dosimeter_network > ~/backup_all_dosenet.sql${NC}"
+   mysqldump -u ne170group -p dosimeter_network > ~/backup_all_dosenet.sql
 elif [[ $CHOICE = "stations" ]]; then
    printf "%s\n" "~ Just backing up the stations table of the dosimeter_network database"
-   printf "%b\n" "~~ Using: ${BLUE}`mysqldump -u ne170group -p dosimeter_network stations > ~/backup_stations_dosenet.sql`"
+   printf "%b\n" "\e~~ Using: ${BLUE}mysqldump -u ne170group -p dosimeter_network stations > ~/backup_stations_dosenet.sql"
+   mysqldump -u ne170group -p dosimeter_network stations > ~/backup_stations_dosenet.sql
 elif [[ $CHOICE = "data" ]]; then
    printf "%s\n" "~ Just backing up the dosnet (data) table of the dosimeter_network database"
-   printf "%b\n" "~~ Using: ${BLUE}`mysqldump -u ne170group -p dosimeter_network dosnet > ~/backup_dosnet_dosenet.sql`"
+   printf "%b\n" "\e~~ Using: ${BLUE}mysqldump -u ne170group -p dosimeter_network dosnet > ~/backup_dosnet_dosenet.sql"
+   mysqldump -u ne170group -p dosimeter_network dosnet > ~/backup_dosnet_dosenet.sql
 else
-   printf "%b\n" "${RED}INVALID INPUT.${NC}"
-   printf "%b\n" "${GREEN}Valid options: all, stations or data${NC}"
-   printf "%s\n" "Try again!?"
+   printf "\t%b\n" "${RED}INVALID INPUT.${NC}"
+   printf "%b\n" "Valid options: ${GREEN}all, stations or data${NC} \n ${BLUE}Try again!?${NC}"
 fi
