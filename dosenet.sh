9c41d7caea110bdd86235aa810e1ed4804f48b27
Navrit Bal <navritbal@gmail.com>
2015-07-30 16:50:18 -0700

    Initial commit - RPi service ...
    Goes in /etc/init.d
    Is called by: /etc/init.d/dosenet start|stop
    Starts UDP_sender.py with a hardcoded config file

---
+++ dosenet.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+# /etc/init.d/dosenet
+### BEGIN INIT INFO
+# Provides:          dosenet
+# Required-Start:    networking
+# Required-Stop:     networking
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# X-Interactive:     false
+# Short-Description: DoseNet - sends UDP packets to the GRIM for the DoseNet project
+### END INIT INFO
+HOME=/home/pi
+DOSENET=$HOME/dosenet
+CONDA=$HOME/miniconda/bin
+LOG=/home/pi/sender.log
+echo "DoseNet script called @ " > $LOG
+date >> $LOG
+case "$1" in
+  start)
+    echo "Starting DoseNet script" >> $LOG
+    $CONDA/python $DOSENET/udp_sender.py -f $DOSENET/config-files/etchhall.csv >> $LOG &
+    date >> $LOG
+    ;;
+  stop)
+    echo "Stopping DoseNet script"  >> $LOG
+    killall python
+    ;;
+  *)
+    echo "Usage: /etc/init.d/dosenet {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
