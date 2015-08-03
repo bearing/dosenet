7f58056739ebcca595988a0d857fe0efd455568f
Navrit Bal <navritbal@gmail.com>
2015-08-03 14:51:46 -0700

    debugging

--- dosimeter.py
+++ dosimeter.py
@@ -206,6 +206,7 @@ if __name__ == "__main__":
     while True:
         try: # getCPM
             sleep(1)
+            GPIO.remove_event_detect(24)
             GPIO.add_event_detect(24, GPIO.FALLING, callback = det.updateCount_basic, bouncetime=200)
             cpm, cpm_err = det.getCPM(accumulation_time = MEASURE_TIME)
             print '\t','CPM: ',cpm,u'±',cpm_err,'\n'
