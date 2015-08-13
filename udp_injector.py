58c4867e29cc0f76250bd57a20802f2a318c20ea
Navrit Bal <navritbal@gmail.com>
2015-08-13 15:31:36 -0700

    Print key file list

--- udp_injector.py
+++ udp_injector.py
@@ -69,7 +69,7 @@ class Injector:
         privateKey = ['/home/dosenet/.ssh/id_rsa.pub']
         de = ccrypt.public_d_encrypt(key_file_lst = privateKey) # Uses 1 private key
         print privateKey
-        print de
+        print de.key_file_lst
         self.port = 5005
         # Gets actual IP address
         self.IP = ([(s.connect(('8.8.8.8', 80)),
