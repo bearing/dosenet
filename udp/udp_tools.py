2cd517491829ea5371dbc97d3ee614de14876c83
Navrit Bal <navritbal@gmail.com>
2015-08-13 15:47:14 -0700

    Diagnostics

--- udp/udp_tools.py
+++ udp/udp_tools.py
@@ -6,11 +6,8 @@ class custSocket:
         self.ip = ip
         self.port = port
         self.decrypt = decrypt
-        print decrypt
-        print decrypt.key
-        print decrypt.private_key
-        if( not decrypt.private_key ):
-            print 'Warning: You will not be able to decrypt messages'
+        #if( not decrypt.private_key ):
+        #    print 'Warning: You will not be able to decrypt messages'
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP ONLY
         try:
             self.sock.bind((ip,port))
