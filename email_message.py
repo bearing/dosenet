2ffd82bd6ed318556d90487a81ce24d0a336bd8b
Navrit Bal <navritbal@gmail.com>
2015-08-16 15:24:25 -0700

    HTML did not render

--- email_message.py
+++ email_message.py
@@ -24,30 +24,34 @@ def send_email(process, error_message):
     Content-type: text/html
     Subject: DoseNet automated message
 
-    <body>
-    <h1> Some DoseNet process just stopped! :( </h1>
-        <h2> Which process stopped? </h2>
-            <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;">{}</code>
-            <br><br>
-            <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
-        <p> Include running/last run process list. </p>
-        <br>
-        <h2> GeoJSON file properties </h2>
-            <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> stat ~/output.geojson </code>
-            <br><br>
-            <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
-        <h2> Which Python processes are running </h2>
-            <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> ps aux | grep python | grep -v grep </code>
-            <br><br>
-            <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
-        <h2> Crontab entries </h2>
-            <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> crontab -l </code>
-            <br><br>
-            <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
-        <br>
-        <p> Navrit Bal </p>
-        <p> Maker of DoseNet. </p>
-    </body>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+    <html>
+        <head></head>
+        <body>
+        <h1> Some DoseNet process just stopped! :( </h1>
+            <h2> Which process stopped? </h2>
+                <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;">{}</code>
+                <br><br>
+                <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
+            <p> Include running/last run process list. </p>
+            <br>
+            <h2> GeoJSON file properties </h2>
+                <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> stat ~/output.geojson </code>
+                <br><br>
+                <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
+            <h2> Which Python processes are running </h2>
+                <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> ps aux | grep python | grep -v grep </code>
+                <br><br>
+                <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
+            <h2> Crontab entries </h2>
+                <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> crontab -l </code>
+                <br><br>
+                <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
+            <br>
+            <p> Navrit Bal </p>
+            <p> Maker of DoseNet. </p>
+        </body>
+    </html>
     """.format(stopped, error_message, geojson, processes, crontab)
 
     try:
