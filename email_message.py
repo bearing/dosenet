2545166f05f1d86e88227a920fad2d50a3a5541d
Navrit Bal <navritbal@gmail.com>
2015-08-15 20:57:42 -0700

    Send HTML & removed comma in my name

--- email_message.py
+++ email_message.py
@@ -6,10 +6,16 @@ sender = 'dosenet@lbl.gov'
 receivers = ['nbal@lbl.gov','ucbdosenet@gmail.com']
 
 message = """From: LBL DoseNet <dosenet@lbl.gov>
-To: Navrit Bal <nbal@lbl.gov>, DoseNet GMail <ucbdosenet@gmail.com>
+To: Navrit Bal <nbal@lbl.gov> DoseNet GMail <ucbdosenet@gmail.com>
+MIME-Version: 1.0
+Content-type: text/html
 Subject: DoseNet automated message
 
-This is a test e-mail message.
+<h1> Some DoseNet process isn't running! :( </h1>
+<p> String format this later to inject the relevant process that isn't running. </p>
+<p> Note: include running/last run process list. </p>
+<p> stat ~/output.geojson? </p>
+<p> ps aux | grep python </p>
 """
 
 try:
