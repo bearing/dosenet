79c5ab5a27108503c7e085c7f6a20f3b2daf3ae8
Navrit Bal <navritbal@gmail.com>
2015-08-16 15:22:35 -0700

    Inline CSS processed with http://premailer.dialect.ca/

--- email_message.py
+++ email_message.py
@@ -24,6 +24,46 @@ def send_email(process, error_message):
     Content-type: text/html
     Subject: DoseNet automated message
 
+    <body>
+    <h1> Some DoseNet process just stopped! :( </h1>
+        <h2> Which process stopped? </h2>
+            <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;">{}</code>
+            <br><br>
+            <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
+        <p> Include running/last run process list. </p>
+        <br>
+        <h2> GeoJSON file properties </h2>
+            <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> stat ~/output.geojson </code>
+            <br><br>
+            <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
+        <h2> Which Python processes are running </h2>
+            <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> ps aux | grep python | grep -v grep </code>
+            <br><br>
+            <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
+        <h2> Crontab entries </h2>
+            <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> crontab -l </code>
+            <br><br>
+            <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
+        <br>
+        <p> Navrit Bal </p>
+        <p> Maker of DoseNet. </p>
+    </body>
+    """.format(stopped, error_message, geojson, processes, crontab)
+
+    try:
+       smtpObj = smtplib.SMTP('localhost')
+       smtpObj.sendmail(sender, receivers, message)
+       print "Successfully sent email"
+    except SMTPException:
+       print "Error: unable to send email"
+
+if __name__ == "__main__":
+    send_email(process = "Test send_email function", error_message = "... Testing email script ...")
+
+
+"""
+PREPROCESSED HTML - EDIT HERE THEN USE http://premailer.dialect.ca/ to convert css to inline
+
     <style type="text/css">
         samp {
             background-color: #f8f8ff;
@@ -66,14 +106,5 @@ def send_email(process, error_message):
             <br>
             <p> Navrit Bal </p>
             <p> Maker of DoseNet. </p>
-    """.format(stopped, error_message, geojson, processes, crontab)
 
-    try:
-       smtpObj = smtplib.SMTP('localhost')
-       smtpObj.sendmail(sender, receivers, message)
-       print "Successfully sent email"
-    except SMTPException:
-       print "Error: unable to send email"
-
-if __name__ == "__main__":
-    send_email(process = "Test send_email function", error_message = "... Testing email script ...")
+"""
