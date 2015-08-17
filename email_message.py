92fb839206901839975ceb8761e8a67ddbfefe64
Navrit Bal <navritbal@gmail.com>
2015-08-16 22:09:56 -0700

    Removing inline CSS

--- email_message.py
+++ email_message.py
@@ -20,31 +20,34 @@ def send_email(process, error_message):
 
     message = """From: LBL DoseNet <dosenet@lbl.gov>
     To: Navrit Bal <nbal@lbl.gov> DoseNet GMail <ucbdosenet@gmail.com>
-    Content-type: text/html
     Subject: DoseNet automated message
 
     <h1> Some DoseNet process just stopped! :( </h1>
-    <h2> Which process stopped? </h2>
-        <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;">{}</code>
-        <br><br>
-        <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
-    <p> Include running/last run process list. </p>
-    <br>
-    <h2> GeoJSON file properties </h2>
-        <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> stat ~/output.geojson </code>
-        <br><br>
-        <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
-    <h2> Which Python processes are running </h2>
-        <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> ps aux | grep python | grep -v grep </code>
-        <br><br>
-        <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
-    <h2> Crontab entries </h2>
-        <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> crontab -l </code>
-        <br><br>
-        <samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
-    <br>
-    <p> Navrit Bal </p>
-    <p> Maker of DoseNet. </p>
+        <h2> Which process stopped? </h2>
+            <code>{}</code>
+            <br>
+            <br>
+            <samp>{}</samp>
+        <p> Include running/last run process list. </p>
+        <br>
+        <h2> GeoJSON file properties </h2>
+            <code> stat ~/output.geojson </code>
+            <br>
+            <br>
+            <samp>{}</samp>
+        <h2> Which Python processes are running </h2>
+            <code> ps aux | grep python | grep -v grep </code>
+            <br>
+            <br>
+            <samp>{}</samp>
+        <h2> Crontab entries </h2>
+            <code> crontab -l </code>
+            <br>
+            <br>
+            <samp>{}</samp>
+        <br>
+        <p> Navrit Bal </p>
+        <p> Maker of DoseNet. </p>
     """.format(stopped, error_message, geojson, processes, crontab)
 
     try:
