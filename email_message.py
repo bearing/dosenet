b3d093ab4fc7077bcc8f7ca32365fdd35c93f4dc
Navrit Bal <navritbal@gmail.com>
2015-08-30 14:43:31 -0700

    Cleanup

--- email_message.py
+++ email_message.py
@@ -38,7 +38,6 @@ def send_email(process, error_message, sender = 'dosenet@dosenet'):
             <h2> Which process stopped? </h2>
                 <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;">{}</code>
                 <br><br><samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
-            <p> Include running/last run process list. </p>
             <br><h2> GeoJSON file properties </h2>
                 <code style="background-color: #f8f8ff; padding-right: 20px; padding-left: 20px; padding-bottom: 7px; border-radius: 10px; font-size: 1.5em; margin: 20px auto; border: 2px solid #8ac007;"> stat ~/output.geojson </code>
                 <br><br><samp style="background-color: #f8f8ff; border-radius: 10px;">{}</samp>
@@ -53,6 +52,7 @@ def send_email(process, error_message, sender = 'dosenet@dosenet'):
 </body>
 </html>""".format(stopped, error_message, geojson, processes, crontab)
 
+    # Include running/last run process listl later?
     part1 = MIMEText(text, 'plain')
     part2 = MIMEText(html, 'html')
     msg.attach(part1)
@@ -70,52 +70,3 @@ def send_email(process, error_message, sender = 'dosenet@dosenet'):
 
 if __name__ == "__main__":
     send_email(process = "Test send_email function", error_message = "... Testing email script ...")
-
-
-"""
-PREPROCESSED HTML - EDIT HERE THEN USE http://premailer.dialect.ca/ to convert css to inline
-
-    <style type="text/css">
-        samp {
-            background-color: #f8f8ff;
-            border-radius: 10px;
-        },
-        code {
-            background-color: #f8f8ff;
-            margin: 20px auto 20px auto;
-            padding-right: 20px;
-            padding-left: 20px;
-            padding-bottom: 7px;
-            border: 2px solid #8AC007;
-            border-radius: 10px;
-            font-size: 1.5em;
-        }
-    </style>
-        <h1> Some DoseNet process just stopped! :( </h1>
-            <h2> Which process stopped? </h2>
-                <code>{}</code>
-                <br>
-                <br>
-                <samp>{}</samp>
-            <p> Include running/last run process list. </p>
-            <br>
-            <h2> GeoJSON file properties </h2>
-                <code> stat ~/output.geojson </code>
-                <br>
-                <br>
-                <samp>{}</samp>
-            <h2> Which Python processes are running </h2>
-                <code> ps aux | grep python | grep -v grep </code>
-                <br>
-                <br>
-                <samp>{}</samp>
-            <h2> Crontab entries </h2>
-                <code> crontab -l </code>
-                <br>
-                <br>
-                <samp>{}</samp>
-            <br>
-            <p> Navrit Bal </p>
-            <p> Maker of DoseNet. </p>
-
-"""
