337fc817af11bee716a0956fdc979b1c93090802
alihanks <alihanks@gmail.com>
2016-03-21 16:27:36 -0700

    updating script for new use case

--- addDosimeterToNetwork.sh
+++ addDosimeterToNetwork.sh
@@ -5,34 +5,16 @@
 
 . ~/.keychain/$HOSTNAME-sh
 
-while [[ $# > 1 ]]
-do
-key="$1"
+numargs=$#
 
-case $key in
-    --name)
-    NAME="$2"
-    NICKNAME="$3"
-    shift # past argument
-    ;;
-    --latlong)
-    LAT="$2"
-    LONG="$3"
-    shift # past argument
-    ;;
-    --conv)
-    CONV="$2"
-    shift # past argument
-    ;;
-    --display)
-    DISPLAY="$2"
-    ;;
-    *)
-       # unknown option
-    ;;
-esac
-shift # past argument or value
-done
+
+
+NAME=$1
+NICKNAME=$2
+LAT=$3
+LONG=$4
+CONV=$5
+DISPLAY=$6
 
 echo "NAME = ${NAME}"
 echo "NICKNAME = ${NICKNAME}"
@@ -42,12 +24,16 @@ echo "CONV = ${CONV}"
 echo "DISPLAY = ${DISPLAY}"
 
 echo "Adding new station to database"
-python ~/git/dosenet_dev/addDosimeterToDB.py --name '$NAME' '$NICKNAME' --latlong $LAT $LONG --conv $CONV --display $DISPLAY
+python ~/git/dosenet_dev/addDosimeterToDB.py "${NAME}" "${NICKNAME}" $LAT $LONG $CONV $DISPLAY
 
 echo "Stopping udp_injector"
 sudo killall python &
 
 echo "Restarting udp_injector"
-screen -dm python udp_injector.py
+#screen -dm python udp_injector.py
+tmux new-session -d -s UDP_INJECTOR '~/anaconda/bin/python udp_injector.py'
+
+#echo "killing all old tmux sessions..."
+#tmux kill-session -a
 
 echo "New station added!"
