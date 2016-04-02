34b35f1985fe276e9e753e884a018788d1703207
dosenet <ucbdosenet@gmail.com>
2016-04-01 19:48:15 -0700

    PEP updates, future print, and import BUG fixes.

--- mysql/mysql_tools.py
+++ mysql/mysql_tools.py
@@ -1,22 +1,30 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import MySQLdb as mdb
-import datetime
 import email_message
 import pandas as pd
-import time
-from dateutil.relativedelta import *
+import os
+import sys
+from dateutil.relativedelta import relativedelta
 
 class SQLObject:
     def __init__(self):
-        self.db = mdb.connect('127.0.0.1','ne170group','ne170groupSpring2015','dosimeter_network')
+        # NOTE should eventually update names (jccurtis)
+        self.db = mdb.connect(
+            '127.0.0.1',
+            'ne170group',
+            'ne170groupSpring2015',
+            'dosimeter_network')
         self.verified_stations = []
         self.cursor = self.db.cursor()
         self.getVerifiedStationList()
 
     def __del__(self):
         self.close()
+
     def __exit__(self):
         self.close()
+
     def close(self):
         self.db.close()
 
@@ -27,10 +35,11 @@ class SQLObject:
         except Exception as e:
             raise e
             msg = 'Error: Could not get list of stations from the database!'
-            print msg
-            email_message.send_email(process = os.path.basename(__file__), error_message = msg)
+            print(msg)
+            email_message.send_email(
+                process=os.path.basename(__file__), error_message=msg)
 
-    def checkHashFromRAM(self,ID):
+    def checkHashFromRAM(self, ID):
         # Essentially the same as doing the following in MySQL
         # "SELECT IDLatLongHash FROM stations WHERE `ID` = $$$ ;"
         try:
@@ -42,69 +51,74 @@ class SQLObject:
             raise e
             return False
             msg = 'Error: Could not find a station matching that ID'
-            print msg
-            email_message.send_email(process = os.path.basename(__file__), error_message = msg)
+            print(msg)
+            email_message.send_email(
+                process=os.path.basename(__file__), error_message=msg)
 
-    def insertIntoDosenet(self,stationID,cpm,cpm_error,error_flag):
-        self.cursor.execute("INSERT INTO dosnet(stationID, cpm, cpmError, errorFlag) \
-                VALUES ('%s','%s','%s','%s');" %
-                (stationID,cpm,cpm_error,error_flag))
+    def insertIntoDosenet(self, stationID, cpm, cpm_error, error_flag):
+        self.cursor.execute(
+            "INSERT INTO dosnet(stationID, cpm, cpmError, errorFlag) \
+             VALUES ('%s','%s','%s','%s');".format(
+                stationID, cpm, cpm_error, error_flag))
         # Time is decided by the MySQL database / DoseNet hence 'receiveTime' field in DB
         self.db.commit()
 
-    def inject(self,data):
+    def inject(self, data):
         if not self.authenticatePacket(data):
-            print '~~ FAILED AUTHENTICATION ~~'
-            print 'Data: ', data
-            msg = str('FAILED AUTHENTICATION\n',data)
-            email_message.send_email(process = os.path.basename(__file__), error_message = msg)
+            print('~~ FAILED AUTHENTICATION ~~')
+            print('Data: ', data)
+            msg = str('FAILED AUTHENTICATION\n', data)
+            email_message.send_email(
+                process=os.path.basename(__file__), error_message=msg)
         else:
             data = self.parsePacket(data)
             if(data):
-                if data[2] > 100: # if cpm > 100
+                if data[2] > 100:  # if cpm > 100
                     msg = 'CPM more than 100 - assumed to be noise event.\n NOT INJECTING.'
-                    print msg
-                    email_message.send_email(process = os.path.basename(__file__), error_message = msg)
+                    print(msg)
+                    email_message.send_email(
+                        process=os.path.basename(__file__), error_message=msg)
                 else:
-                    self.insertIntoDosenet(
-                        stationID   = data[1],
-                        cpm         = data[2],
-                        cpm_error   = data[3],
-                        error_flag  = data[4])
+                    self.insertIntoDosenet(stationID=data[1],
+                                           cpm=data[2],
+                                           cpm_error=data[3],
+                                           error_flag=data[4])
             else:
-                print '~~ FAILED TO INJECT/PARSE ~~'
-                msg = '\t stationID: "%s", cpm: "%s", cpm_error: "%s", error_flag: "%s"' % \
-                    (stationID,cpm,cpm_error,error_flag)
-                print msg
-                email_message.send_email(process = os.path.basename(__file__), error_message = msg)
+                msg = '~~ FAILED TO INJECT/PARSE ~~'
+                print(msg)
+                email_message.send_email(
+                    process=os.path.basename(__file__), error_message=msg)
 
     def getHashList(self):
         return self.verified_stations
 
-    def authenticatePacket(self,data):
+    def authenticatePacket(self, data):
         '''
         Checks hash in hash list and compares against ID.
         '''
         hash_list = self.getHashList()
-        msg_hash = data[:32] # Is it the correct hash length?
-        if not any(str(msg_hash) in i for i in hash_list): # Verify the hash is in the list
-            print 'Message Hash: ', msg_hash
-            print 'Hash list: ', hash_list
-            print 'Hash is not in list'
+        # Is it the correct hash length?
+        msg_hash = data[:32]
+        # Verify the hash is in the list
+        if not any(str(msg_hash) in i for i in hash_list):
+            print('Message Hash:', msg_hash)
+            print('Hash list:', hash_list)
+            print('Hash is not in list')
             return False
         # Ok, we think this could be a real station
         ID       = int(data.split(',')[1])
         db_hash  = self.checkHashFromRAM(ID)
-        if db_hash == msg_hash: # Is it an authenticated station with a matching database entry?
+        # Is it an authenticated station with a matching database entry?
+        if db_hash == msg_hash:
             return True
         else:
-            print 'ID:', ID, 'Hash from database: ', db_hash, 'Hash from message', msg_hash
-            return False # Valid data, not authenticated station.
+            print('ID:', ID, 'Hash from database:', db_hash, 'Hash from message', msg_hash)
+            return False  # Valid data, not authenticated station.
 
-    def parsePacket(self,data):
-        data = data.split(',') # Commas are our delimiters for the decrypted message
+    def parsePacket(self, data):
+        data = data.split(',')  # Commas are our delimiters for the decrypted message
         msg_hash = data[0]
-        if len(msg_hash) == 32: # All is good. Cast as known data types
+        if len(msg_hash) == 32:  # All is good. Cast as known data types
             stationID   = int(data[1])
             cpm         = float(data[2])
             cpm_error   = float(data[3])
@@ -112,24 +126,24 @@ class SQLObject:
             return (msg_hash, stationID, cpm, cpm_error, error_flag)
         else:
             return False
-            print 'Data:', data
-            print 'Message hash', msg_hash
-            print 'Station ID: "%s", CPM: "%s", CPM error: "%s", Error flag: "%s"' \
-                % (stationID, cpm, cpm_error, error_flag)
+            print('Data:', data)
+            print('Message hash', msg_hash)
+            print('Station ID: "{}", CPM: "{}", CPM error: "{}", Error flag: "{}"'.format(
+                stationID, cpm, cpm_error, error_flag))
 
     def getHashFromDB(self, ID):
         # RUN "SELECT IDLatLongHash FROM stations WHERE `ID` = $$$ ;"
         try:
             self.cursor.execute("SELECT IDLatLongHash FROM stations \
-                            WHERE `ID` = '%s';" % (ID) )
+                            WHERE `ID` = '%s';" % (ID))
             dbHash = self.cursor.fetchall()
             return dbHash
         except (KeyboardInterrupt, SystemExit):
-            print ('User interrupted for some reason, byeeeee')
+            print('User interrupted for some reason, byeeeee')
             sys.exit(0)
         except (Exception) as e:
-            print str(e)
-            print ('Exception: Could not get hash from database. Is GRIM online and running MySQL?')
+            print(e)
+            print('Exception: Could not get hash from database. Is GRIM online and running MySQL?')
 
     def getStations(self):
         df = pd.read_sql(
