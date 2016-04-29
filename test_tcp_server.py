9c62a39c0c3e6e4d65034d32364688f7543b0798
Brian Plimley <as.white.as.snow@gmail.com>
2016-04-29 01:10:21 -0700

    test_tcp_server.py (first go)

---
+++ test_tcp_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import socket
+import time
+
+
+server_address = 'dosenet.dhcp.lbl.gov'
+server_port = 6900
+# commons.lbl.gov/pages/viewpage.action?spaceKey=cpp&title=Perimeter+Protection
+#   for open TCP ports on an LBL wired DHCP computer. doesn't have to be 6900
+
+buffer_size = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((server_address, server_port))
+
+# listen() sets the socket up as serverside
+#   the number is the max number of connections.
+s.listen(25)
+print('Waiting for connections...')
+
+# accept() takes a single connection, returning a new socket representing
+#   that specific connection.
+# accept() blocks until a connection is made.
+conn, addr = s.accept()
+print('Got a connection from {}'.format(addr))
+try:
+    while True:
+        print('Waiting for data...')
+        data = conn.recvfrom(buffer_size)
+        if data:
+            print('  Received data: {}'.format(data))
+        else:
+            time.sleep(1)
+            # does this work?
+finally:
+    conn.close()
