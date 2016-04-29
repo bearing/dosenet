7b788ba239b6682aa306aa93229b11ec2cc13ecc
Brian Plimley <as.white.as.snow@gmail.com>
2016-04-29 02:07:08 -0700

    rewriting test_tcp_server using SocketServer

--- test_tcp_server.py
+++ test_tcp_server.py
@@ -3,6 +3,7 @@
 
 from __future__ import print_function
 import socket
+import SocketServer
 import time
 
 
@@ -11,36 +12,15 @@ server_port = 6900
 # commons.lbl.gov/pages/viewpage.action?spaceKey=cpp&title=Perimeter+Protection
 #   for open TCP ports on an LBL wired DHCP computer. doesn't have to be 6900
 
-buffer_size = 1024
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((server_address, server_port))
+class MyHandler(SocketServer.BaseRequestHandler):
+    """Define the handle() method to handle requests"""
 
-# listen() sets the socket up as serverside
-#   the number is the max number of connections.
-s.listen(25)
+    def handle(self):
+        data = self.request.recv(1024)
+        print('  {} sent}: {}'.format(self.client_address, data))
 
-while True:
-    print('Waiting for connections...')
 
-    # accept() takes a single connection, returning a new socket representing
-    #   that specific connection.
-    # accept() blocks until a connection is made.
-    conn, addr = s.accept()
-    print('Got a connection from {}'.format(addr))
-
-    # This only handles a single connection! ~~~> see SocketServer
-    try:
-        while True:
-            print('Waiting for data...')
-            data, addr2 = conn.recvfrom(buffer_size)
-            # conn.recvfrom blocks execution until data arrives - IF connection
-            #   is still alive
-            if data:
-                print('  Received data from {}: {}'.format(addr2, data))
-            else:
-                # Apparently this means the connection was closed
-                break
-
-    finally:
-        conn.close()
+if __name__ == '__main__':
+    server = SocketServer.TCPServer((server_address, server_port), MyHandler)
+    server.serve_forever()
