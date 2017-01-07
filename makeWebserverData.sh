5ad4d0fb9dc1d958832e2fb1fac4ddb6f43dcea5
Brian Plimley <as.white.as.snow@gmail.com>
2017-01-06 16:15:43 -0800

    tidy up data_transfer.py and makeWebserverData.sh

--- makeWebserverData.sh
+++ makeWebserverData.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-. ~/.keychain/$HOSTNAME-sh
-~/anaconda/bin/python ~/git/dosenet/makeCSV.py
-~/anaconda/bin/python ~/git/dosenet/makeGeoJSON.py
-~/anaconda/bin/python ~/git/dosenet/sendDataToWebserver.py
+LOGTAG=makeWebserverData
+HOMEPATH=/home/dosenet
+PYTHONPATH=$HOMEPATH/anaconda/bin/
+PATH=$PYTHONPATH:$PATH
+
+. $HOMEPATH/.keychain/$HOSTNAME-sh
+
+python $HOMEPATH/git/dosenet/makeCSV.py
+python $HOMEPATH/git/dosenet/makeGeoJSON.py
+python $HOMEPATH/git/dosenet/sendDataToWebserver.py
+
+logger --stderr --id --tag $LOGTAG "Finished sending data to webserver"
+
