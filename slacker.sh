8173532519a4583f5cf9363fecc7f0d211d9b01c
Brian Plimley <as.white.as.snow@gmail.com>
2017-02-10 15:54:51 -0800

    slacker.sh for cron

---
+++ slacker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# runs slacker.py
+
+LOGTAG=Slack
+SLACKER_PATH=/home/dosenet/git/dosenet_slack_outage
+
+logger --stderr --id --tag=$LOGTAG "Starting slacker.py..."
+
+python $SLACKER_PATH/slacker.py &
+
+if [[ $? -eq 0 ]]
+then
+  logger --stderr --id --tag=$LOGTAG "Successfully started slacker.py"
+else
+  logger --stderr --id --tag=$LOGTAG "Failed to start slacker.py"
+fi
