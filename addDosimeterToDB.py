696800e0f6c53a46282c2319634035395dde0c0a
alihanks <alihanks@gmail.com>
2017-03-14 12:28:22 -0700

    update string formatting

--- addDosimeterToDB.py
+++ addDosimeterToDB.py
@@ -98,28 +98,27 @@ class DBTool:
 
         # Adds a row to dosimeter_network.stations
         sql = ("INSERT INTO stations " +
-               "(`ID`,`Name`,`Lat`,`Long`,`cpmtorem`,`cpmtousv`,"+
+               "(`ID`,`Name`,`Lat`,`Long`,`cpmtorem`,`cpmtousv`," +
                "`display`,`devices`,IDLatLongHash,`nickname`,`timezone`) " +
                "VALUES " +
-               "('%s','%s','%s','%s','%s','%s','%s','%s'," +
-               "'This should not be here :(','%s','%s');"
-               % (self.ID,self.name, self.lat, self.lon, self.cpmtorem,
-                  self.cpmtousv, self.display, self.devices, self.nickname,
-                  self.timezone))
+               "('{}','{}','{}','{}','{}','{}','{}','{}',".format(
+                    self.ID, self.name, self.lat, self.lon, self.cpmtorem,
+                    self.cpmtousv, self.display, self.devices) +
+               "'This should not be here :(','{}','{}');".format(
+                    self.nickname,self.timezone))
         self.runSQL(sql)
         self.main()
 
     def addDosimeterWithID(self):
-        sql = (
-            "INSERT INTO stations " +
-            "(`ID`,`Name`,`Lat`,`Long`,`cpmtorem`,`cpmtousv`," +
-            "`display`,`devices`,IDLatLongHash,`nickname`,`timezone`) " +
-            "VALUES " +
-            "('%s','%s','%s','%s','%s','%s','%s','%s',"+
-            "'This should not be here :(','%s','%s');"
-            % (self.ID, self.name, self.lat, self.lon, self.cpmtorem,
-               self.cpmtousv, self.display, self.devices, self.nickname,
-               self.timezone))
+        sql = ("INSERT INTO stations " +
+               "(`ID`,`Name`,`Lat`,`Long`,`cpmtorem`,`cpmtousv`," +
+               "`display`,`devices`,IDLatLongHash,`nickname`,`timezone`) " +
+               "VALUES " +
+               "('{}','{}','{}','{}','{}','{}','{}','{}',".format(
+                    self.ID, self.name, self.lat, self.lon, self.cpmtorem,
+                    self.cpmtousv, self.display, self.devices) +
+               "'This should not be here :(','{}','{}');".format(
+                    self.nickname,self.timezone))
         self.runSQL(sql)
         self.main()
 
