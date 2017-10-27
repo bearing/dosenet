0cd519ca5cd922d3c3e6db530de356cdb506e3d5
cllamb0 <cllamb777@gmail.com>
2017-10-27 12:12:53 -0700

    Trying to make the length testing more robust

--- injector.py
+++ injector.py
@@ -488,7 +488,7 @@ class Injector(object):
             request_type = 'co2'
             print("This is working")
             print(len(field_list[3]))
-            print(field_list[3])
+            print(field_list[3][1])
         elif len(field_list) == num_data_fields_old:
             request_type = 'data_old'
         elif len(field_list) == num_data_fields_new:
