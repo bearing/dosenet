7fdba31bd6bb583beab6ca5bb80cc5502256fb06
alihanks <alihanks@gmail.com>
2017-11-25 16:40:24 -0800

    minor bug fix

--- makeCSV.py
+++ makeCSV.py
@@ -209,7 +209,7 @@ def get_compressed_adc_data(DB,sid,integration_time,n_intervals):
 
     comp_df = pd.DataFrame(columns=['deviceTime_unix','cpm','cpmError'])
     for idx in range(n_intervals):
-        df = DB.getWeatherDataForStationByRange(sid,max_time - interval,max_time)
+        df = DB.getADCDataForStationByRange(sid,max_time - interval,max_time)
         max_time = max_time - interval
         if len(df) > 0:
             comp_df.loc[idx,'deviceTime_unix'] = df.iloc[len(df)/2,0]
