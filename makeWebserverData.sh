5ddc442a9896faa8073a6547aca0e28e19602cc9
alihanks <alihanks@gmail.com>
2019-01-15 21:57:25 -0800

    update to script to run everything based on input arguments

--- makeWebserverData.sh
+++ makeWebserverData.sh
@@ -8,10 +8,36 @@ PATH=$PYTHONPATH:$PATH
 . $HOMEPATH/.keychain/$HOSTNAME-sh
 
 # let devices post first
-sleep 60
 logger --stderr --id --tag $LOGTAG "Making files for webserver..."
+for arg in "$@"
+do
+    if [ "$arg" == "--geojson" ]
+    then
+        python $HOMEPATH/git/dosenet/makeGeoJSON.py
+    fi
+    if [ "$arg" == "--last-hour" ]
+    then
+        python $HOMEPATH/git/dosenet/makeCSV.py
+    fi
+    if [ "$arg" == "--last-day" ]
+    then
+        python $HOMEPATH/git/dosenet/makeCSV.py --last-day
+    fi
+    if [ "$arg" == "--last-week" ]
+    then
+        python $HOMEPATH/git/dosenet/makeCSV.py --last-week
+    fi
+    if [ "$arg" == "--last-month" ]
+    then
+        python $HOMEPATH/git/dosenet/makeCSV.py --last-month
+    fi
+    if [ "$arg" == "--last-year" ]
+    then
+        python $HOMEPATH/git/dosenet/makeCSV.py --last-year
+    fi
+done
 #python $HOMEPATH/git/dosenet/makeCSV.py
-python $HOMEPATH/git/dosenet/makeGeoJSON.py
+#python $HOMEPATH/git/dosenet/makeGeoJSON.py
 python $HOMEPATH/git/dosenet/sendDataToWebserver.py
 
 logger --stderr --id --tag $LOGTAG "Finished sending data to webserver"
