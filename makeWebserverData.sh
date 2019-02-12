7e49fd812d8fa39eda6d00a8c402fa19bcbc3d76
alihanks <alihanks@gmail.com>
2019-02-11 17:32:47 -0800

    bug fix attempt

--- makeWebserverData.sh
+++ makeWebserverData.sh
@@ -6,21 +6,19 @@ PATH=$PYTHONPATH:$PATH
 
 . $HOMEPATH/.keychain/$HOSTNAME-sh
 
+args=("$@")
 
-while [ 1 < 2 ]; do
-  for arg in "$@"
-  do
-      if [ "$arg" == "--geojson" ]
-      then
-          sleep 30
-          python $HOMEPATH/git/dosenet/makeGeoJSON.py
-      fi
-      if [ "$arg" == "--data" ]
-      then
-          sleep 150
-          python $HOMEPATH/git/dosenet/makeCSV.py
-      fi
-  done
+while [ 1 ]; do
+  if [ "${args[0]}" == "--geojson" ]
+  then
+      sleep 30
+      python $HOMEPATH/git/dosenet/makeGeoJSON.py
+  fi
+  if [ "${args[0]}" == "--data" ]
+  then
+      sleep 150
+      python $HOMEPATH/git/dosenet/makeCSV.py
+  fi
 
   python $HOMEPATH/git/dosenet/sendDataToWebserver.py
   echo "Finished sending data to webserver"
