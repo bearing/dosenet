b79bff2bee57894e11b629458d199623fbed8a7b
alihanks <alihanks@gmail.com>
2019-02-12 16:21:20 -0800

    minor changes

--- database_queries.py
+++ database_queries.py
@@ -3,5 +3,6 @@ from mysql_tools.mysql_tools import SQLObject
 
 def get_stations():
     DB = SQLObject()
-    df= DB.getStations()
-    print(df)
+    df = DB.getStations()
+    df_sub = df[['Name','Lat','Long']]
+    print(df_sub)
