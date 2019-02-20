d60bbfc7e735f033a1731379c2a9f29616b1a0b8
alihanks <alihanks@gmail.com>
2019-02-19 16:29:15 -0800

    testing

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import os
-from functools import wraps
+import functools
 import errno
 import os
 import signal
@@ -12,16 +12,19 @@ def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
         def _handle_timeout(signum, frame):
             raise TimeoutError(error_message)
 
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             signal.signal(signal.SIGALRM, _handle_timeout)
             signal.alarm(seconds)
             try:
                 result = func(*args, **kwargs)
+            except TimeoutError:
+                pass
             finally:
                 signal.alarm(0)
             return result
 
-        return wraps(func)(wrapper)
+        return wrapper
 
     return decorator
 
