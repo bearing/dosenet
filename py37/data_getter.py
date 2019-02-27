bfdd0737c5127e7fcccc798f041c8ded8d7caa0c
sagnibak <sagnikdps@gmail.com>
2019-02-27 11:56:31 -0800

    made new files

---
+++ py37/data_getter.py
@@ -0,0 +1,3 @@
+"""This module has methods to get data from the Raspberry Pis and
+decrypt the data for storage.
+"""
---
+++ py37/injector.py
@@ -0,0 +1,3 @@
+"""This script uses the module `data_getter.py` to acquire and decrypt
+data coming from the Raspberry Pis, and then writes them to `.csv` files.
+"""
