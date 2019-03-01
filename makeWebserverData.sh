cf6b395edee9d5e32877f6299fdd1fd1d39cf182
alihanks <alihanks@gmail.com>
2019-03-01 11:21:32 -0800

    point to correct code version in wrapper script

--- makeWebserverData.sh
+++ makeWebserverData.sh
@@ -3,6 +3,7 @@
 HOMEPATH=/home/dosenet
 PYTHONPATH=$HOMEPATH/anaconda/bin/
 PATH=$PYTHONPATH:$PATH
+CODEPATH=$HOMEPATH/git/dosenet-ali-dev
 
 . $HOMEPATH/.keychain/$HOSTNAME-sh
 
@@ -14,15 +15,15 @@ while [ 1 ]; do
   if [ "${args[0]}" == "--geojson" ]
   then
       sleep 30
-      python $HOMEPATH/git/dosenet/makeGeoJSON.py
+      python $CODEPATH/makeGeoJSON.py
   fi
   if [ "${args[0]}" == "--data" ]
   then
       sleep 150
-      python $HOMEPATH/git/dosenet/makeCSV.py
+      python $CODEPATH/makeCSV.py
   fi
 
-  python $HOMEPATH/git/dosenet/sendDataToWebserver.py
+  python $CODEPATH/sendDataToWebserver.py
   echo "Finished sending data to webserver"
 
   logger --stderr --id --tag $LOGTAG "Finished sending data to webserver"
