e5a0dd03ea4fe4539ac3889bb661534498d3af6b
sagnibak <sagnikdps@gmail.com>
2019-03-11 12:08:38 -0700

    py37/injector.py almost done

--- py37/data_getter.py
+++ py37/data_getter.py
@@ -1,3 +1,27 @@
 """This module has methods to get data from the Raspberry Pis and
 decrypt the data for storage.
 """
+
+def get_data(...):
+    """The function that gets called by `injector.py` to get data.
+    Parameters
+    ----------
+    ...
+    Returns
+    -------
+    ...
+    """
+
+def decrypt_packet(encrypted_packet, ...):
+    """Decrypts one `encrypted_packet` using  AES encryption.
+
+    Parameters
+    ----------
+    encrypted_packet:  the packet to be decrypted
+
+    Returns
+    -------
+    A dictionary containing the decrypted data that can then be stored in
+    a .csv file.
+    """
+
--- py37/injector.py
+++ py37/injector.py
@@ -1,3 +1,15 @@
 """This script uses the module `data_getter.py` to acquire and decrypt
 data coming from the Raspberry Pis, and then writes them to `.csv` files.
 """
+
+def inject_data(field_dict, filename):
+    """
+    Writes the data in `field_dict` to `filename`.
+    
+    Parameters
+    ----------
+    field_dict:  dictionary containing data
+    filename:    location to write the files at; should be .csv
+    """
+    with open(filename, 'w+') as f:
+        # TODO: get field names from ../injector.format_packet(...)
