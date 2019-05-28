decc629108b21a2e7c7e259b3f69118d6c96ba55
alihanks <alihanks@gmail.com>
2019-05-28 16:58:10 -0700

    use temporary python2 env path

--- makeWebserverData.sh
+++ makeWebserverData.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 HOMEPATH=/home/dosenet
-PYTHONPATH=$HOMEPATH/anaconda/bin/
+PYTHONPATH=$HOMEPATH/anaconda3/envs/aws-test-conda2/bin
 PATH=$PYTHONPATH:$PATH
 CODEPATH=$HOMEPATH/git/dosenet-ali-dev
 
 . $HOMEPATH/.keychain/$HOSTNAME-sh
-
-source activate aws-test-conda2
 
 args=("$@")
 
@@ -15,15 +13,15 @@ while [ 1 ]; do
   if [ "${args[0]}" == "--geojson" ]
   then
       sleep 30
-      python $CODEPATH/makeGeoJSON.py
+      $PYTHONPATH/python $CODEPATH/makeGeoJSON.py
   fi
   if [ "${args[0]}" == "--data" ]
   then
       #sleep 150
-      python $CODEPATH/makeCSV.py
+      $PYTHONPATH/python $CODEPATH/makeCSV.py
   fi
 
-  python $CODEPATH/sendDataToWebserver.py
+  $PYTHONPATH/python $CODEPATH/sendDataToWebserver.py
   echo "Finished sending data to webserver"
 
   logger --stderr --id --tag $LOGTAG "Finished sending data to webserver"
