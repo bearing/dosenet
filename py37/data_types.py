c0c7d49d588c0627cec2897a96ce0227203da21d
sagnibak <sagnikdps@gmail.com>
2019-06-14 17:23:23 -0700

    added data types for sensor data and inject method

---
+++ py37/data_types.py
@@ -0,0 +1,75 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Tuple
+from typing_extensions import Protocol
+
+class CSVAble(Protocol):
+    def to_csv(self) -> str: ...
+
+@dataclass(frozen=True)
+class WeatherSensorData:
+    unix_time: int
+    local_time: datetime
+    utc_time: datetime
+    temperature: float
+    pressure: float
+    humidity: float
+
+    def to_csv(self) -> str:
+        return f'{self.utc_time},{self.local_time},{self.unix_time},{self.temperature},{self.pressure},{self.humidity}\n'
+
+
+@dataclass(frozen=True)
+class D3SSensorData:
+    unix_time: int
+    local_time: datetime
+    utc_time: datetime
+    cpm: float
+    cpm_error: float
+    keV_per_channel: float
+    readings: Tuple[int]
+
+    def to_csv(self) -> str:
+        string = f'{self.utc_time},{self.local_time},{self.unix_time},{self.cpm},{self.cpm_error},{self.keV_per_channel}'
+        for reading in self.readings:
+            string += f',{reading}'
+        string += '\n'
+
+        return string
+
+
+@dataclass(frozen=True)
+class PGSensorData:
+    unix_time: int
+    local_time: datetime
+    utc_time: datetime
+    cpm: float
+    cpm_error: float
+
+    def to_csv(self) -> str:
+        return f'{self.utc_time},{self.local_time},{self.unix_time},{self.cpm},{self.cpm_error}\n'
+
+
+@dataclass(frozen=True)
+class AQSensorData:
+    unix_time: int
+    local_time: datetime
+    utc_time: datetime
+    pm_1: float
+    pm_25: float
+    pm_10: float
+
+    def to_csv(self) -> str:
+        return f'{self.utc_time},{self.local_time},{self.unix_time},{self.pm_1},{self.pm_25},{self.pm_10}\n'
+
+
+@dataclass(frozen=True)
+class CO2SensorData:
+    unix_time: int
+    local_time: datetime
+    utc_time: datetime
+    co2_ppm: float
+    noise: float
+
+    def to_csv(self) -> str:
+        return f'{self.utc_time},{self.local_time},{self.unix_time},{self.co2_ppm},{self.noise}\n'
--- py37/injector.py
+++ py37/injector.py
@@ -1,8 +1,12 @@
 """This script uses the module `data_getter.py` to acquire and decrypt
 data coming from the Raspberry Pis, and then writes them to `.csv` files.
 """
+from data_types import *
+from typing import Sequence, Union
+from typing_extensions import Protocol
 
-def inject_data(field_dict, filename):
+def inject_data(data: Sequence[CSVAble],
+                filename: str):
     """
     Writes the data in `field_dict` to `filename`.
     
@@ -13,3 +17,5 @@ def inject_data(field_dict, filename):
     """
     with open(filename, 'w+') as f:
         # TODO: get field names from ../injector.format_packet(...)
+        for line in data:
+            f.write(line.to_csv())
