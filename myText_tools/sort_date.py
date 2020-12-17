d01a26fa481e187beb9a7bdbf72c30344c154fe0
Ethan Chang <mv.echang@berkeley.edu>
2020-12-16 16:44:42 -0800

    sort date file

---
+++ myText_tools/sort_date.py
@@ -0,0 +1,34 @@
+import re
+from os import listdir
+from os.path import isfile, join
+import csv
+import operator
+
+# read in all the csv in a directory and write a function that sort the csv with date
+# sort data function will be given an input file and an output file => sorted output into input
+
+def sort_all(data_path = "/Users/ethanchang/dosenet_data/dosenet", output_dir = "/Users/ethanchang/dosenet/myText_tools/tmp/dosenet"):
+    files = [f for f in listdir(data_path) if isfile(join(data_path, f)) and re.fullmatch('^((?!(year|month|week|day|hour)).)*$', f)]
+    for f in files:
+        sort_csv(join(data_path, f), join(output_dir, f))
+
+def sort_csv(inputfile, outputFile):
+    inf = open(inputfile, "r")
+    ouf = open(outputFile, "w")
+    all = []
+    reader = csv.reader(inf, delimiter=",")
+    # print(all)
+    for item in reader:
+        all.append(item)
+        # print(item)
+    lines = all[1:]
+    header = all[0]
+    # sort by unix time
+    sorted_lines = sorted(lines, key=operator.itemgetter(2), reverse=False)
+    print(",".join(header), file=ouf)
+    for line in sorted_lines:
+        print(",".join(line), file=ouf)
+    inf.close()
+    ouf.close()
+
+sort_all()
