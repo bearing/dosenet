d3c7757bf3ba93903b7f5f7205ce150d120a6327
alihanks <j.ali.hanks@gmail.com>
2023-04-06 15:35:10 -0700

    catch upload exceptions

--- data_upload.py
+++ data_upload.py
@@ -23,34 +23,41 @@ for file in all_files:
 
     CHUNK_SIZE = 4 * 1024 * 1024 #4 megabytes
 
-    if file_size <= CHUNK_SIZE:
-        dbx.files_upload(f.read(), destination_path)
+    try:
+        if file_size <= CHUNK_SIZE:
+            dbx.files_upload(f.read(), destination_path)
 
-    else:
-        upload_session_start_result = dbx.files_upload_session_start(f.read(CHUNK_SIZE)) #read 4mb of the file, start session
-        cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,offset=f.tell())
-        # UploadSessorCursor contains the upload session ID and the offset.
-        # I'm guessing they're setting the initial offset to 0mb (right?) through f.tell()
-        # f.tell(): returns an integer giving the file object’s current position in the file represented as number of bytes from the beginning of the file.
+        else:
+            upload_session_start_result = dbx.files_upload_session_start(f.read(CHUNK_SIZE)) #read 4mb of the file, start session
+            cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,offset=f.tell())
+            # UploadSessorCursor contains the upload session ID and the offset.
+            # I'm guessing they're setting the initial offset to 0mb (right?) through f.tell()
+            # f.tell(): returns an integer giving the file object’s current position in the file represented as number of bytes from the beginning of the file.
 
-        commit = dropbox.files.CommitInfo(path=destination_path)
-        #Contains the path and other optional modifiers for the future upload commit.
+            commit = dropbox.files.CommitInfo(path=destination_path)
+            #Contains the path and other optional modifiers for the future upload commit.
 
-        while f.tell() < file_size:
-            if ((file_size - f.tell()) <= CHUNK_SIZE):
-                #if remaining filesize is less than 4mb
-                print(dbx.files_upload_session_finish(f.read(CHUNK_SIZE),cursor,commit))
-                print('\n\n\ncomplete')
-                #Finish the upload session and save the uploaded data to the given file path.
-            else:
-                dbx.files_upload_session_append_v2(f.read(CHUNK_SIZE),cursor)
-                #append more data to an upload session
-                cursor.offset = f.tell()
-                #offset updated to new byte position in file
-                b_left = round(((file_size-f.tell())/file_size),2)*100
-                b_left = str(b_left)+"%" if b_left < 1000 else 0
-                print("\r       {} remaining....".format(b_left), end="")
-                #fun percentage remaining printing thing
+            while f.tell() < file_size:
+                if ((file_size - f.tell()) <= CHUNK_SIZE):
+                    #if remaining filesize is less than 4mb
+                    print(dbx.files_upload_session_finish(f.read(CHUNK_SIZE),cursor,commit))
+                    print('\n\n\ncomplete')
+                    #Finish the upload session and save the uploaded data to the given file path.
+                else:
+                    dbx.files_upload_session_append_v2(f.read(CHUNK_SIZE),cursor)
+                    #append more data to an upload session
+                    cursor.offset = f.tell()
+                    #offset updated to new byte position in file
+                    b_left = round(((file_size-f.tell())/file_size),2)*100
+                    b_left = str(b_left)+"%" if b_left < 1000 else 0
+                    print("\r       {} remaining....".format(b_left), end="")
+                    #fun percentage remaining printing thing
+    except Exception as e:
+        print("ERROR: file upload failed!")
+        print("")
+        print(e)
+        print("")
+        pass
 
     f.close()
 
